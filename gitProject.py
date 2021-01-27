c4c167d18c6f5a7e3d6e3a125e30d23919a2e7cd
bishan <bishan_rulez@hotmail.com>
2021-01-27 13:47:42 +0000

    display rules

--- gitProject.py
+++ gitProject.py
@@ -7,3 +7,10 @@ def menuChoice():
         print("That is not a valid choice.")
         choice = int(input("Please enter a number between 1 and 3: "))
     return choice
+
+def displayRules():
+    print("The rules of the game are as follows: \n \
+    Players take turns to throw two dice. \n \
+    If the throw is a `double`, i.e. two 2s, two 3s, etc., \n \
+    the player's score reverts to zero and their turn ends. \n \
+    (etc.)")
